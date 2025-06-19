e1003835ffb9be1a81a39ce984198f18ef84cce0
sneha-khambal <snehakhambal0@gmail.com>
2025-06-19 19:16:01 +0530

    both frontend and backend connected and created user in DB with hashPassword

--- backend/main.js
+++ backend/main.js
@@ -2,12 +2,20 @@ import express from "express";
 import bookRoutes from "./routes/booksRoutes.js";
 import mongoose from "mongoose";
 import userAuthRouter from "./routes/userAuthRoutes.js";
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors({ 
+    origin: 'http://localhost:5173' ,  
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['content-type']
+}))
 
 app.use("/", bookRoutes);
 app.use("/user", userAuthRouter); 
+
+
 
 
 
